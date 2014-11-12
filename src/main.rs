643cecb9b47e018434ac7aa4745242699a31409d
Brandon Haley <brandon.haley94@gmail.com>
2014-11-12 11:32:58 -0800

    rudimentry(?) ncurses framework, added ncurses dependency

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+extern crate ncurses;
+
+fn setup(){
+    ncurses::initscr();
+    ncurses::noecho();
+    ncurses::raw();
+    ncurses::keypad(ncurses::stdscr, true);
+}
+
+fn is_running() -> bool{
+    let ch = ncurses::getch();
+    ncurses::printw("%c", ch);
+    if ch == ncurses::KEY_F(1){
+        return false;
+    }
+    return true;
+}
+
+fn main_loop(){
+    ncurses::refresh();
+}
+
+fn clean_up(){
+    ncurses::endwin();
+}
+
 fn main() {
-    println!("Hello, world!")
+    setup();
+    while is_running(){
+        main_loop();
+    }
+    clean_up();
 }
